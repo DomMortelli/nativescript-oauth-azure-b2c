3c5c01e3ccfb7fa19a5b1f2f0e6234e4bbb8f4b3
unknown <dominik.buehler@ecocoach.com>
2018-08-08 00:50:03 +0200

    added js files

---
+++ auth-helper-custom.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var auth_helper_1 = require("./auth-helper");
+var AuthHelperCustom = (function (_super) {
+    __extends(AuthHelperCustom, _super);
+    function AuthHelperCustom(credentials, cookieDomains) {
+        var _this = _super.call(this) || this;
+        _this.cookieDomains = cookieDomains;
+        _this.credentials = {};
+        Object.assign(_this.credentials, credentials);
+        _this.credentials.scope = credentials.scope.replace(/ /g, '%20');
+        return _this;
+    }
+    AuthHelperCustom.prototype.logout = function (successPage) {
+        return this._logout(successPage, this.cookieDomains);
+    };
+    return AuthHelperCustom;
+}(auth_helper_1.AuthHelper));
+exports.AuthHelperCustom = AuthHelperCustom;
